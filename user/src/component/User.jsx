6e5df60d6b9c43b86c639fd85e83f11eaa55af01
agyanim <agyanimconsultancy@gmail.com>
2025-11-27 10:36:17 +0000

    axios and try catch added

--- user/src/component/User.jsx
+++ user/src/component/User.jsx
@@ -1,26 +1,38 @@
 import { useEffect, useState } from "react";
 import HeaderCard from "./header/HeaderCard";
 import MainCard from "./main/MainCard";
-import "../index.css"
+import "../index.css";
+import axios from "axios";
 
 const User = () => {
 	const [user, setUser] = useState({});
-	
-    const getData = () => {
-		fetch("https://randomuser.me/api/")
-			.then((res) => res.json())
-			.then((data) => setUser(data.results[0])
-            )
-	};
-useEffect(()=>{
-    getData()
-},[])
-console.log(user);
 
+	const getData = async () => {
+		try {
+			const res = await axios("https://randomuser.me/api/");
+			setUser(res.data.results[0]);
+		} catch (error) {
+			console.log(error);
+		}
+
+	};
+	useEffect(() => {
+		getData();
+	}, []);
+
+	useEffect(() => {
+		const interId = setInterval(() => {
+			getData();
+		}, 5000);
+
+		return () => {
+			clearInterval(interId);
+		};
+	}, []);
 	return (
 		<div className="user-wrapper">
 			<HeaderCard {...user} />
-			<MainCard {...user} getData={getData}/>
+			<MainCard {...user} getData={getData} />
 		</div>
 	);
 };
