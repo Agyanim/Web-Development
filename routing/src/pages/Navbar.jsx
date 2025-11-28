9df901379603d168d75fdf9a14003be61761cd60
agyanim <agyanimconsultancy@gmail.com>
2025-11-28 09:00:02 +0000

    header component added

--- routing/src/pages/Navbar.jsx
+++ routing/src/pages/Navbar.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
+import Header from '../components/Header'
 
 const Navbar = () => {
   return (
-    <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/contact">Contact</a></li>
-    </ul>
+    <nav>
+      <Header/>
+    </nav>
   )
 }
 
