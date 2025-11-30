4e64487418c19298073db8b157f7fcd995cd722b
agyanim <agyanimconsultancy@gmail.com>
2025-11-30 06:23:54 +0000

    home page added

--- routing/src/pages/Home.jsx
+++ routing/src/pages/Home.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
-import "./pages.css"
+import React from "react";
+import "./pages.css";
+import Courses from "./Courses";
 
 const Home = () => {
-  return (
-    <div>
-      home
-    </div>
-  )
-}
+	return (
+		<div className="home-wrapper">
+				<h1>About</h1>
+			<div className="about-wrapper">
+					<p>
+						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet alias sit doloremque deleniti iusto similique dignissimos minus delectus? Accusamus, dolore!
+					</p>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste, quam delectus. Reiciendis nostrum consequuntur deleniti similique, eius aliquid optio obcaecati?
+					</p>
+			</div>
+			<div className="about-wrapper">
+				<p>
+					Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente quo
+					consequatur odio delectus rerum nostrum magnam vel ex. Consectetur
+					magni doloremque expedita numquam sit molestias maiores perspiciatis
+					nulla, quod amet!
+				</p>
+			</div>
+			<div className="course-wrapper">
+				<h1>Courses</h1>
+				<div>
+					<Courses />
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default Home
+export default Home;
